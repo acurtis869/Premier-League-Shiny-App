7e74108afbbaadd366cee4b36d55b6d24ae6029b
acurtis869 <acurtis869@googlemail.com>
2020-11-03 15:54:11 +0000

    Create initial layout for UI.

--- ui.R
+++ ui.R
@@ -1,3 +1,6 @@
+#ui.R
+
+library(shiny)
 library(tidyverse)
 library(rvest)
 
@@ -53,8 +56,32 @@ points <- htmlPage %>%
   html_text() %>%
   as.numeric()
 
-
-
+fluidPage(
+  
+  # App name
+  titlePanel("Premier League Table"),
+  
+  # Sidebar with options on how to sort, a refresh button and a download data
+  # option
+  sidebarLayout(
+    
+    sidebarPanel(
+      # Sorting options
+      selectInput("ordering", "Order by:",
+                  choices = colnames(premTable)),
+      # Refresh button
+      actionButton(inputId = "refresh",
+                   label = "Refresh Table",
+                   icon = icon("sync")),
+      # Download dataset button
+      actionButton(inputId = "download",
+                   label = "Download Dataset",
+                   icon = icon("download"))
+    ),
+    # The main table
+    mainPanel(plotOutput(outputId = "table"))
+    )
+)
 
 
 premTable <- data.frame(position, club, played, won, 
