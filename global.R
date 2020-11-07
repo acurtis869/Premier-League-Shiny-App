de071c240600aaa547bfd5605201d6a0ae9295fa
acurtis869 <acurtis869@googlemail.com>
2020-11-07 16:06:14 +0000

    Made use of global.R instead of having two extra functions.

---
+++ global.R
@@ -0,0 +1,83 @@
+# Function which scrapes current data for the Premier League from online and
+# returns the table as a data frame
+
+# Load required libraries
+library(tidyverse)
+library(rvest)
+
+getPremTable <- function() {
+  # Begin scraping
+  URL <- "https://www.premierleague.com/tables"
+  htmlPage <- read_html(URL)
+  
+  # Access the table:
+  position <- 1:20
+  
+  club <- htmlPage %>% 
+    html_nodes(".isPL .long") %>%
+    html_text()
+  
+  played <- htmlPage %>% 
+    html_nodes(".isPL .team+ td") %>%
+    html_text() %>%
+    as.numeric()
+  
+  won <- htmlPage %>%
+    html_nodes(".isPL td:nth-child(5)") %>%
+    html_text() %>%
+    as.numeric()
+  
+  drawn <- htmlPage %>%
+    html_nodes(".isPL td:nth-child(6)") %>%
+    html_text() %>%
+    as.numeric()
+  
+  lost <- htmlPage %>%
+    html_nodes(".isPL td:nth-child(6)") %>%
+    html_text() %>%
+    as.numeric()
+  
+  GF <- htmlPage %>%
+    html_nodes(".isPL .hideSmall:nth-child(8)") %>%
+    html_text() %>%
+    as.numeric()
+  
+  GA <- htmlPage %>%
+    html_nodes(".isPL .hideSmall+ .hideSmall") %>%
+    html_text() %>%
+    as.numeric()
+  
+  GD <- GF - GA
+  
+  points <- htmlPage %>%
+    html_nodes(".isPL .points") %>%
+    html_text() %>%
+    as.numeric()
+  
+  # Compile data into a data frame
+  premTable <- data.frame(position, club, played, won, 
+                          drawn, lost, GF, GA, GD, points)
+  
+  return(premTable)
+}
+
+# Function created for modularity which defines the ouputs required in the 
+# server.
+
+# Load required library
+library(lubridate)
+
+getOutput <- function(input, output) {
+  # Scrape data using scraping function above
+  premTable <- getPremTable()
+  # Define our main output - a table
+  output$table <- renderTable(
+    arrange(premTable, 
+            as.numeric(input$desc) * 
+              dplyr::desc(!!rlang::sym(input$ordering))),
+    digits = 0)
+  # Output the time of the last update
+  output$time <- renderText({
+    print(paste0("Table last updated at ", as_datetime(Sys.time())))
+  })
+}
--- server.R
+++ server.R
@@ -1,38 +1,20 @@
 # server.R
-library(lubridate)
 
 function(input, output) {
-  # Create function (for modularity) which retrieves data and time and outputs
-  # it in the correct format
-  getOutput <- function() {
-    # Scrape data using external function
-    source("getPremTable.R")
-    premTable <- getPremTable()
-    # Define our main output - a table
-    output$table <- renderTable(
-      arrange(premTable, 
-              as.numeric(input$desc) * 
-                dplyr::desc(!!rlang::sym(input$ordering))),
-      digits = 0)
-    # Output the time of the last update
-    output$time <- renderText({
-      print(paste0("Table last updated at ", as_datetime(Sys.time())))
-    })
-  }
   
   # Create timer which triggers refresh every hour
   reload <- reactiveTimer(3.6e+6)
   # Code will rerun once timer reaches an hour
   observe({
     reload()
-    # Retrive and output data using function above
-    getOutput()
+    # Retrive and output data using function in global.R
+    getOutput(input, output)
   })
   
   # Refresh table using the button
   observeEvent(input$refresh, {
-    # Output updated data using the function defined above
-    getOutput()
+    # Output updated data using the function defined in global.R
+    getOutput(input, output)
   })
   
   # Download dataset button
