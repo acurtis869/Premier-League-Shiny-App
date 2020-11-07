2ba667ba822ab694f90b7619a5551f694bf7b746
acurtis869 <acurtis869@googlemail.com>
2020-11-07 15:31:24 +0000

    Made a function within server to make code more modular.

--- server.R
+++ server.R
@@ -2,33 +2,30 @@
 library(lubridate)
 
 function(input, output) {
-  # Scrape data from Premier League website
-  source("getPremTable.R")
-  premTable <- getPremTable()
-  
-  # Define our main output - a table
-  output$table <- renderTable(
-    arrange(premTable, 
-            as.numeric(input$desc) * 
-              dplyr::desc(!!rlang::sym(input$ordering))),
-    digits = 0)
-  
-  output$time <- renderText({
-    print(paste0("Table last updated at ", as_datetime(Sys.time())))
-    })
-  
-  # Refresh table using the button
-  observeEvent(input$refresh, {
+  # Create function (for modularity) which retrieves data and time and outputs
+  # it in the correct format
+  getData <- function() {
+    # Scrape data using external function
+    source("getPremTable.R")
     premTable <- getPremTable()
+    # Define our main output - a table
     output$table <- renderTable(
       arrange(premTable, 
               as.numeric(input$desc) * 
                 dplyr::desc(!!rlang::sym(input$ordering))),
       digits = 0)
-    
+    # Output the time of the last update
     output$time <- renderText({
       print(paste0("Table last updated at ", as_datetime(Sys.time())))
     })
+  }
+  #
+  getData()
+
+  
+  # Refresh table using the button
+  observeEvent(input$refresh, {
+    getData()
   })
   
   # Download dataset button
