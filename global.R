2803b5391be84e81ffd68efe299598f038717961
Julian Leopold <julian@rega-sense.ch>
2020-11-13 15:36:54 +0100

    restructuring files

--- global.R
+++ global.R
@@ -1,89 +1,52 @@
-# Function which scrapes current data for the Premier League from online and
-# returns the table as a data frame
+#######################################################################
+# This file calls the data scrapers, prepares the data, and passes it
+# as plots to the shiny application.
+#
+# Imports: scarper functions
+# 
+# University of St Andrews, 2020
+#######################################################################
 
-# Load required libraries
+# Load required libraries -------------------------------------------------
+
 library(tidyverse)
 library(rvest)
 library(ggdark)
+library(lubridate) # for parsing premier league data.
+# library(measurments) # for coordinates
 
-getPremTable <- function() {
-  # Begin scraping
-  URL <- "https://www.premierleague.com/tables"
-  htmlPage <- read_html(URL)
-  
-  # Access the table:
-  position <- 1:20
-  
-  club <- htmlPage %>% 
-    html_nodes(".isPL .long") %>%
-    html_text()
-  
-  played <- htmlPage %>% 
-    html_nodes(".isPL .team+ td") %>%
-    html_text() %>%
-    as.numeric()
-  
-  won <- htmlPage %>%
-    html_nodes(".isPL td:nth-child(5)") %>%
-    html_text() %>%
-    as.numeric()
-  
-  drawn <- htmlPage %>%
-    html_nodes(".isPL td:nth-child(6)") %>%
-    html_text() %>%
-    as.numeric()
-  
-  lost <- htmlPage %>%
-    html_nodes(".isPL td:nth-child(6)") %>%
-    html_text() %>%
-    as.numeric()
-  
-  GF <- htmlPage %>%
-    html_nodes(".isPL .hideSmall:nth-child(8)") %>%
-    html_text() %>%
-    as.numeric()
-  
-  GA <- htmlPage %>%
-    html_nodes(".isPL .hideSmall+ .hideSmall") %>%
-    html_text() %>%
-    as.numeric()
-  
-  GD <- GF - GA
-  
-  points <- htmlPage %>%
-    html_nodes(".isPL .points") %>%
-    html_text() %>%
-    as.numeric()
-  
-  # Compile data into a data frame
-  premTable <- data.frame(position, club, played, won, 
-                          drawn, lost, GF, GA, GD, points)
-  
-  return(premTable)
-}
 
-# Function created for modularity which defines the ouputs required in the 
-# server.
+# Load Required Scraper Functions -----------------------------------------
 
-# Load required library
-library(lubridate)
+source("scraper/getPremTable.R")
+source("scraper/getMarketValue.R")
+source("scraper/getCoords.R")
+
+# Generate Data and plots For Shiny Application --------------------------
 
 getOutput <- function(input, output) {
   # Scrape data using scraping function above
   premTable <- getPremTable()
+
+# Table -------------------------------------------------------------------
+
   # Define our main output - a table
   output$table <- renderTable(
     arrange(premTable, 
             as.numeric(input$desc) * 
               dplyr::desc(!!rlang::sym(input$ordering))),
     digits = 0)
+  
+
+# Print Time Updated ------------------------------------------------------
+  
   # Output the time of the last update
   output$time <- renderText({
     print(paste0("Table last updated at ", as_datetime(Sys.time())))
   })
   
-  
-  # Output the Scatter Plot
+# Scatter Plot ------------------------------------------------------------
+
   output$scatterPlot <- renderPlot({
     # create data frame
     df <- data.frame(premTable)
@@ -92,11 +55,14 @@ getOutput <- function(input, output) {
       geom_point() + 
       geom_smooth() +
       dark_theme_dark() +
-      theme(plot.background = element_rect(fill = "#343E48", colour = "#343E48"))
+      theme(plot.background = element_rect(fill = "#343E48", 
+                                           colour = "#343E48"))
     },
     height = 400, width = 600)
   
-  # Create map content
+# Map Content -------------------------------------------------------------
+
+  # Prepare Data
   stadiums <- data.frame(
     lat = c(
       30.42106667,
@@ -115,6 +81,7 @@ getOutput <- function(input, output) {
     ),
     radius = c(1,20,3,10,5,50))
   
+  # render Plot
   output$mymap <- renderLeaflet({
     leaflet(data = stadiums) %>%
       addProviderTiles(providers$OpenStreetMap,
@@ -128,48 +95,4 @@ getOutput <- function(input, output) {
         label = stadiums$radius
         )
   })
-}
-
-
-# Get market value --------------------------------------------------------
-
-getMarketValue <- function() {
-  # Begin scraping
-  URL <- "https://www.transfermarkt.com/premier-league/marktwerteverein/wettbewerb/GB1"
-  htmlPage <- read_html(URL)
-  
-  value_raw <- htmlPage %>% 
-    html_nodes(".rechts.hauptlink a") %>%
-    html_text()
-  value_raw <- value_raw[seq(2, 40, 2)] %>%
-    str_remove("€")
-  value <- rep(NA, length(value_raw))
-  for (i in 1:length(value_raw)) {
-    if (grepl("bn", value_raw[i])) {
-      value_raw[i] <- str_remove(value_raw[i], "bn")
-      value[i] <- as.numeric(value_raw[i])
-      value[i] <- value[i] * 1000
-    }
-    else {
-      value_raw[i] <- str_remove(value_raw[i], "m")
-      value[i] <- as.numeric(value_raw[i])
-    }
-  } 
-  
-  club <- htmlPage %>%
-    html_nodes(".no-border-links") %>%
-    html_text() %>%
-    str_remove(" FC") %>%
-    str_replace("&", "and")
-  
-  MarketValue <- data.frame(club, value)
-  return(MarketValue)
-}
-
-mergeTables <- function() {
-  premTable <- getPremTable()
-  MarketValue <- getMarketValue()
-
-  merged <- merge(premTable, MarketValue)
-  return(merged)
 }
---
+++ scraper/getPremTable.R
@@ -0,0 +1,61 @@
+#######################################################################
+# This function scrapes data from the premier league.
+# 
+# University of St Andrews, 2020
+#######################################################################
+
+getPremTable <- function() {
+  # Begin scraping
+  URL <- "https://www.premierleague.com/tables"
+  htmlPage <- read_html(URL)
+  
+  # Access the table:
+  position <- 1:20
+  
+  club <- htmlPage %>% 
+    html_nodes(".isPL .long") %>%
+    html_text()
+  
+  played <- htmlPage %>% 
+    html_nodes(".isPL .team+ td") %>%
+    html_text() %>%
+    as.numeric()
+  
+  won <- htmlPage %>%
+    html_nodes(".isPL td:nth-child(5)") %>%
+    html_text() %>%
+    as.numeric()
+  
+  drawn <- htmlPage %>%
+    html_nodes(".isPL td:nth-child(6)") %>%
+    html_text() %>%
+    as.numeric()
+  
+  lost <- htmlPage %>%
+    html_nodes(".isPL td:nth-child(6)") %>%
+    html_text() %>%
+    as.numeric()
+  
+  GF <- htmlPage %>%
+    html_nodes(".isPL .hideSmall:nth-child(8)") %>%
+    html_text() %>%
+    as.numeric()
+  
+  GA <- htmlPage %>%
+    html_nodes(".isPL .hideSmall+ .hideSmall") %>%
+    html_text() %>%
+    as.numeric()
+  
+  GD <- GF - GA
+  
+  points <- htmlPage %>%
+    html_nodes(".isPL .points") %>%
+    html_text() %>%
+    as.numeric()
+  
+  # Compile data into a data frame
+  premTable <- data.frame(position, club, played, won, 
+                          drawn, lost, GF, GA, GD, points)
+  
+  return(premTable)
+}
